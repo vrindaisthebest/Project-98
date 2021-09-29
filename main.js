3e3fb2cf8227ec4760543de3d7a9227b7ac92ce5
vrindaisthebest <88569278+vrindaisthebest@users.noreply.github.com>
2021-09-29 15:17:12 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var speechrecognition = window.webkitSpeechRecognition;
+var recognition = new speechrecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult =  function (event) {
+
+    console.log (event);
+    var content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = content;
+    console.log(content);
+
+    if (content == "take my selfie") {
+        console.log("taking selfie");
+        speak();
+
+    }
+}
+
